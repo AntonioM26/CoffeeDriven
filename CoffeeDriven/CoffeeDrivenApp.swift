f3c0aacd4d31eb001542f450782494c2d4da2b9f
António Almeida <antonio95@icloud.com>
2023-04-18 22:46:34 +0100

    Initial Commit

---
+++ CoffeeDriven/CoffeeDrivenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeDrivenApp.swift
+//  CoffeeDriven
+//
+//  Created by António Almeida on 18/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeDrivenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
